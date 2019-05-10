de87e8c4221c243b78a62578efadaa24df7c510b
gtorche@gmail.com <gtorche@gmail.com>
2019-05-10 13:36:03 -0700

    Fix Flow types for block headers

--- src/engine/engineState.js
+++ src/engine/engineState.js
@@ -659,7 +659,7 @@ export class EngineState extends EventEmitter {
         const queryTime = Date.now()
         return fetchBlockHeader(
           parseInt(height),
-          (header: any) => {
+          (header: StratumBlockHeader) => {
             console.log(`${prefix} received header for block number ${height}`)
             this.fetchingHeaders[height] = false
             this.pluginState.serverScoreUp(uri, Date.now() - queryTime)
@@ -986,7 +986,7 @@ export class EngineState extends EventEmitter {
   }
 
   // A server has sent a header, so update the cache and txs:
-  handleHeaderFetch (height: string, header: any) {
+  handleHeaderFetch (height: string, header: StratumBlockHeader) {
     if (!this.pluginState.headerCache[height]) {
       this.pluginState.headerCache[height] = header
       const affectedTXIDS = this.findAffectedTransactions(height)
